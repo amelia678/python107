9c7f1b33d61e4bddb93062fc5b9c88c5724c57a4
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-09-25 13:38:50 -0400

    adds comments

--- word_histogram.py
+++ word_histogram.py
@@ -4,14 +4,14 @@
 sentence = input("Please enter a sentence:").lower()
  
 def histogram(str):
-    counts = {}
-    words = str.split()
+    counts = {} #empty dictionary
+    words = str.split() #splits string into words
 
     for word in words:
         if word in counts:
-            counts[word] += 1
+            counts[word] += 1 #if word is already in dictionary, add count
         else:
-            counts[word] = 1
+            counts[word] = 1 # if word is not in dictionary, add key and value of 1
     return counts            
 
 print(histogram(sentence))
